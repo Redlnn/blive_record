67b43a3a1c460d1acd2676314b4d3e4d284bed69
Redlnn <w731347477@gmail.com>
2021-04-08 18:33:40 +0800

    [perf]提升启动脚本的运行效果

--- start.bat
+++ start.bat
@@ -1,5 +1,8 @@
 @echo off
 set a=%~s0
 cd %a%\..
+cls
 python blive_record.py
-pause
+echo.
+echo 程序已退出，请按任意键退出...
+pause>nul
--- start.sh
+++ start.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env sh
 DIR=$(cd `dirname $0`; pwd)
 cd $DIR
+clear
 python3 blive_record.py
