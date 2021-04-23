2944c62cb587c9b2aeed91d1a5616bc8bacd0cb0
Redlnn <w731347477@gmail.com>
2021-04-24 00:43:47 +0800

    [new]分离配置与本体

--- blive_record.py
+++ blive_record.py
@@ -26,21 +26,8 @@ from subprocess import PIPE, Popen, STDOUT
 import requests
 from regex import match
 
-"""
-*------------以下为可配置项-------------*
-"""
-# room_id = 1151716  # 莴苣某人
-# room_id = 1857249  # Red_lnn
-room_id = 1151716  # 要录制的B站直播间的直播间ID
-segment_time = 1800  # 录播分段时长（单位：秒，大于等于1小时可能会造成 FFmpeg 异常退出录制）
-check_time = 60  # 开播检测间隔（单位：秒）
-file_extensions = 'flv'  # 录制文件后缀名（文件格式）
-verbose = True  # 是否打印ffmpeg输出信息到控制台
-debug = False  # 是否显示并保存调试信息（优先级高于 verbose）
-save_log = True  # 是否保存日志信息为文件，同一天多次启动本脚本会共用同一个日志文件，每天凌晨分割一次日志文件
-"""
-*------------以上为可配置项-------------*
-"""
+# 导入配置
+from config import *   # noqa
 
 record_status = False  # 录制状态，True为录制中
 kill_times = 0  # 尝试强制结束FFmpeg的次数
