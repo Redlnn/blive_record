1ff0055bcfde164b4a887de21764cf4c70849be6
尘绪呀~ <41742767+Redlnn@users.noreply.github.com>
2021-05-02 23:56:56 +0800

    [perf]微调flv2mp4.bat中的命令

--- flv2mp4.bat
+++ flv2mp4.bat
@@ -1,3 +1,3 @@
 @echo off
 if not exist ".\convert" (mkdir convert)
-for %%a in (".\download\*.flv") do ffmpeg -i "%%a" -c:v copy -c:a copy ".\convert\%%~na.mp4"
+for %%a in (".\download\*.flv") do ffmpeg -i "%%a" -c copy ".\convert\%%~na.mp4"
